4415023f06d070b82287ed492001931459d5bf06
zenzen <81463856+zenzen-k@users.noreply.github.com>
2023-01-29 16:15:32 +0900

    Update 잘라서 배열로 저장하기.java

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -2,18 +2,18 @@ class Solution {
     public String[] solution(String my_str, int n) {
         int num = (int) Math.ceil((double) my_str.length() / n);
 			
-		String[] answer = new String[num];
+	String[] answer = new String[num];
 
-		int num2 = 0;
+	int num2 = 0;
 		
-		for(int i=0; i<my_str.length(); i+=n) {
-			if(i+n > my_str.length()) {
-				answer[num2] = my_str.substring(i);
-				break;
-			}
-			answer[num2] = my_str.substring(i,i+n);
-			num2 ++;
+	for(int i=0; i<my_str.length(); i+=n) {
+		if(i+n > my_str.length()) {
+			answer[num2] = my_str.substring(i);
+			break;
 		}
+		answer[num2] = my_str.substring(i,i+n);
+		num2 ++;
+	}
         return answer;
     }
 }
