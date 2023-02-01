068e2d4c81e2032278ee64de538448d0e7dd13d5
zenzen <81463856+zenzen-k@users.noreply.github.com>
2023-02-01 21:49:29 +0900

    Update 문자열 정렬하기 （2）.java

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -28,3 +28,14 @@ class Solution {
         return answer;
     }
 }
+
+// 배열 정렬 함수 이용한 방법은 아래
+// 이제는 이렇게 다양한 함수이용법도 알아두자!
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        char[] ch = my_string.toLowerCase().toCharArray();
+        Arrays.sort(ch);
+        return new String(ch);
+    }
+}
