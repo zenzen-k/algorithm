3decf81f5caf9ed5af8d71f9bcc0b7085acd7250
zenzen <81463856+zenzen-k@users.noreply.github.com>
2023-02-01 22:02:26 +0900

    [Bronze V] Title: A+B - 8, Time: 136 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for(int i=1; i<=t; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A+B));
+		}
+	}
+}
