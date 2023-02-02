ed65a277fead5c4006c7dc65bea96397681b438b
zenzen <81463856+zenzen-k@users.noreply.github.com>
2023-02-02 22:05:58 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.06 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,15 +1,6 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        double d = Math.sqrt(n);
-        int i = (int)Math.sqrt(n);
-        
-        if(d==i)
-        	answer = 1;
-        else
-        	answer = 2;
-
-        return answer;
+        return Math.sqrt(n)%1==0 ? 1 : 2;
     }
 }
