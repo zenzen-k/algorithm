8fc2955285391918c95065d8d9c9bed68dc98ab2
zenzen <81463856+zenzen-k@users.noreply.github.com>
2023-02-07 21:29:21 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        String[] sp = {};
+        
+        
+        for(int i=0; i<quiz.length; i++) {
+        	sp = quiz[i].split(" ");
+        	int a = 0;
+        	if(sp[1].equals("-"))
+        		a = Integer.parseInt(sp[0]) - Integer.parseInt(sp[2]);
+        	else if(sp[1].equals("+"))
+        		a = Integer.parseInt(sp[0]) + Integer.parseInt(sp[2]);
+        	
+        	if(Integer.parseInt(sp[4]) == a)
+        		answer[i] = "O";
+        	else
+        		answer[i] = "X";
+        }
+
+        return answer;
+    }
+}
