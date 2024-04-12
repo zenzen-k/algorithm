9df43a4ed0b0dff71284ab102716d914db7d7753
김아연 <81463856+zenzen-k@users.noreply.github.com>
2024-04-13 07:08:39 +0900

    [Bronze II] Title: 진법 변환, Time: 212 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String b = sc.next();
+		int n = sc.nextInt();
+		
+		System.out.println(Integer.parseInt(b, n));
+
+	}
+}
