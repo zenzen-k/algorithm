d8677e986557fd33d4d8de0598ebc040f59618bf
김아연 <81463856+zenzen-k@users.noreply.github.com>
2024-04-22 13:55:06 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 220 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args)   {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Long n = sc.nextLong();
+		
+		System.out.println(n*n*n);
+		System.out.println(3);
+		
+		sc.close();
+		
+	}
+}
