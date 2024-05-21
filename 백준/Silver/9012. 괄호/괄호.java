3f9816e3c6d49a9f8aba9f5a51107ab2bb540e82
김아연 <81463856+zenzen-k@users.noreply.github.com>
2024-05-22 07:29:14 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class Main {
@@ -11,30 +10,36 @@ public class Main {
 		
 		int k = Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<k; i++) {
-			Stack<Integer> stackStr = new Stack<>();
-			String str = br.readLine();
-			
-			boolean flag = true;
-			
-			for(int j=0; j<str.length(); j++) {
-				if(str.charAt(j) == ('(')) {
-					stackStr.push(1);
-				}else {
-					try {
-						stackStr.pop();
-					} catch (EmptyStackException e) {
-						flag = false;
-						break;
-					}
-				}
-			}
-			
-			sb.append((stackStr.isEmpty()&&flag ? "YES" : "NO")+ "\n");
+		for(int i = 0; i < k; i++) {
+			sb.append(solve(br.readLine())).append('\n');
 		}
-		
 		System.out.println(sb);
 		br.close();
 	}
-
+	
+	
+	public static String solve(String s) {
+		Stack<Character> stack = new Stack<>();
+ 
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+ 
+			if (c == '(') {
+				stack.push(c);
+			}
+			else if (stack.empty()) {
+				return "NO";
+			}
+			else {
+				stack.pop();
+			}
+		}
+ 
+		if (stack.empty()) {
+			return "YES";
+		} 
+		else {
+			return "NO";
+		}
+	}
 }
