4f2da8e62a1fd63257a06c3c8f49d4010bd69159
김아연 <81463856+zenzen-k@users.noreply.github.com>
2024-05-30 21:17:18 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14404 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		long sum = 0;
-		
 		String str = br.readLine();
+		BigInteger sum = new BigInteger("0");
+		
 		for(int i=0; i<n; i++) {
-			char ch = str.charAt(i);
-			sum +=  ((int)ch - 96) * Math.pow(31, i);
+			sum = sum.add(BigInteger.valueOf(str.charAt(i) - 96).multiply(BigInteger.valueOf(31).pow(i)));
 		}
 		
-		System.out.println(sum);
-		
+		System.out.println(sum.remainder(BigInteger.valueOf(1234567891)));
 		br.close();
 	}
 
