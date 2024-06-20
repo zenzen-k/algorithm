24979338749cd284e82afd6c54b848d1c8953734
김아연 <81463856+zenzen-k@users.noreply.github.com>
2024-06-20 11:53:53 +0900

    [Silver V] Title: 공포의 면담실, Time: 112 ms, Memory: 14420 KB, Score: 36 point -BaekjoonHub

---
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int t = Integer.parseInt(st.nextToken());
+			for(int j=0; j<t; j++) {
+				arr[i] += Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		Arrays.sort(arr);
+		int sum = 0;
+		for(int i=0; i<n; i++) {
+			if(i != 0) {
+				arr[i] += arr[i-1];
+			}
+			sum += arr[i];
+		}
+		
+		System.out.println(sum);
+		br.close();
+	}
+
+}
